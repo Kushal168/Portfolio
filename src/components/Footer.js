987ec0cd8c6c90d288bf47c153d27dcf234bba1f
Kushal <Kushalgoyal168@gmail.com>
2023-09-06 22:53:01 +0530

    adding the Hire Me button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,11 @@ const Footer = () => {
     font-medium text-lg'
     >
         <Layout className='py-8 flex items-center justify-between'>
-            <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
-            <div className='flex items-center'>
+            <span className='font-semibold'>{new Date().getFullYear()} &copy; All Rights Reserved</span>
+            <div className='flex items-center font-semibold'>
             Build with <span className='text-primary text-2xl px-1'>&#9825;</span> by<Link  className='underline underline-offset-2' href="/">Kushal</Link>
             </div>
-            <Link href="https://www.linkedin.com/in/kushal-goyal-316367207" target='blank' className='underline underline-offset-2'>Say Hello</Link>
+            <Link href="https://www.linkedin.com/in/kushal-goyal-316367207" target='blank' className='underline underline-offset-2 font-semibold'>Say Hello</Link>
         </Layout>
     </footer>
   )
