e2d017676be16ff9d7451f1ab27a055c9255c3fc
Kushal <Kushalgoyal168@gmail.com>
2023-09-06 22:25:15 +0530

    change the bg of webdeveloper svg 1:20

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import profilePic from "../../public/images/profile/developer-pic-1.png";
 import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
 import { LinkArrow } from "@/components/Icons";
+import HireMe from "@/components/HireMe";
 
 export default function Home() {
   return (
@@ -39,6 +40,8 @@ export default function Home() {
             </div>
           </div>
         </Layout>
+
+        <HireMe/>
       </main>
     </>
   );
