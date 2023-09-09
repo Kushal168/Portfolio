65dc444c11204caa77d9612fc42c1e844f5220c2
Kushal <Kushalgoyal168@gmail.com>
2023-09-09 21:50:55 +0530

    Animate the skill and completed 90%

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,8 +8,9 @@ return (
    shadow-dark cursor-pointer '
   whileHover={{scale:1.05}}
   initial={{x:0,y:0}}
-  animate={{x:x,y:y}}
+  whileInView={{x:x,y:y}}
   transition={{duration:1.5}}
+  viewport={{once:true}}
   >
     {name}
   </motion.div>
@@ -23,22 +24,23 @@ const Skills = () => {
    <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight pl-50'>
     <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 absolute shadow-dark cursor-pointer'
     whileHover={{scale:1.05}}
+
     >
       Web 
     </motion.div>
 
     {/* <Skill name="Web" x="0" y="0"/> */}
 
-    <Skill name="HTML" x="-20vw" y="2vw"/>
-    <Skill name="CSS" x="-5vw" y="-10vw"/>
-    <Skill name="Javascript" x="20vw" y="6vw"/>
-    <Skill name="ReactJS" x="0vw" y="12vw"/>
-    <Skill name="NextJS" x="-20vw" y="-15vw"/>
+    <Skill name="HTML" x="-2vw" y="12vw"/>
+    <Skill name="CSS" x="5vw" y="-2vw"/>
+    <Skill name="Javascript" x="25vw" y="10vw"/>
+    <Skill name="ReactJS" x="-1vw" y="17vw"/>
+    <Skill name="NextJS" x="-18vw" y="-15vw"/>
     <Skill name="NodeJS" x="15vw" y="-12vw"/>
     <Skill name="ExpressJS" x="32vw" y="-5vw"/>
     <Skill name="MongoDB" x="0vw" y="-20vw"/>
-    <Skill name="Data " x="-25vw" y="-18vw"/>
-    <Skill name="Tailwind CSS" x="18vw" y="18vw"/>
+    <Skill name="Data Structure " x="-30vw" y="-15vw"/>
+    <Skill name="Tailwind CSS" x="-4vw" y="10vw"/>
    </div>
    </>
   )
