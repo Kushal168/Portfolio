df34308ea97b9b3cf7e3c13665acf2cf58ae615e
Kushal <Kushalgoyal168@gmail.com>
2023-09-11 00:42:22 +0530

    Styling Projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,42 @@
 import AnimatedText from '@/components/AnimatedText'
+import { Github } from '@/components/Icons'
 import Layout from '@/components/Layout'
+import { Festive } from 'next/font/google'
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import projects1 from "../../public/images/projects/crypto-screener-cover-image.jpg"
+
+
+const FeaturedProjects = ({type, title, summary, img, link, github}) =>{
+    return(
+       <article className='w-full flex items-center justify-between rounded-3xl border border-solid border-dark
+       bg-light shadow-2xl p-12
+       '>
+        <Link href={link} target="blank"
+        className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
+        >
+            <Image src={img} alt={title} className='w-full h-auto'></Image>
+        </Link>
+            <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
+                <span className='text-primary font medium text-xl'> {type} </span>
+                <Link href={link} target="blank" className='hover:underline underline-offset-2'>
+                    <h2 className='my-2 w-full text-left text-4xl font-bold '>{title}</h2>
+                </Link>
+                <p className='my-2 font-medium text-dark'>{summary}</p>
+                <div className='mt-2 flex items-center'>
+                <Link className='w-10' href={github} target="blank"> <Github/>  </Link>
+                <Link className='ml-4 rounded-lg bg-dark text-light px-6 text-lg font-semibold w-30' href={link} target="blank">Visit Project</Link>
+                </div>
+            </div>
+       </article>
+    )
+}
+
 
 const projects = () => {
   return (
-   
       <>
       <Head>
         <title>Kushal | About page</title>
@@ -17,13 +48,35 @@ const projects = () => {
 
             <div className='grid grid-cols-12 gap-24 '>
                 <div className='col-span-12'>
+                <FeaturedProjects title="Crypto Screener Application"
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+                It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                local currency."
+                link="/"
+                type="Featured Project"
+                github="https://github.com/Kushal168"
+                img={projects1}
+
+                />
+                </div>
+
+
+
+                <div className='col-span-6'>
+                    Project - 1
+                </div>
+                <div className='col-span-6'>
+                    Project - 2
+                </div>
+
+                <div className='col-span-12'>
                     Featured Project
                 </div>
                 <div className='col-span-6'>
-                    Featured Project
+                    Project - 3
                 </div>
                 <div className='col-span-6'>
-                    Featured Project
+                    Project - 4
                 </div>
             </div>
 
