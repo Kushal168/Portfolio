896a1504a9203516668b2b9b18fc9684ad058f5b
Kushal <Kushalgoyal168@gmail.com>
2023-09-11 22:26:42 +0530

    style

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -34,6 +34,35 @@ const FeaturedProjects = ({type, title, summary, img, link, github}) =>{
     )
 }
 
+const Project = ({title, type, img, link, github })=>{
+    return(
+        <article className='w-full flex flex-col items-center justify-center rounded-2xl border border-solid bg-light p-6 relative'>
+              <Link href={link} target="blank"
+        className='w-full cursor-pointer overflow-hidden rounded-lg'
+        >
+            <Image src={img} alt={title} className='w-full h-auto'></Image>
+        </Link>
+            <div className='w-full flex flex-col items-start justify-between mt-4 '>
+                <span className='text-primary font medium text-xl'> {type} </span>
+                <Link href={link} target="blank" className='hover:underline underline-offset-2'>
+                    <h2 className='my-2 w-full text-left text-3xl font-bold '>{title}</h2>
+                </Link>
+               
+                <div className=' w-full mt-2 flex items-center justify-between'>
+
+                <Link className=' text-10 font-semibold w-30 h-10 pt-2 hover:underline' href={link} target="blank">
+                    Visit 
+                </Link>
+
+                <Link className='w-8'href={github} target="blank"> {" "}
+                 <Github/>{" "} 
+                </Link>
+                </div>
+            </div>
+        </article>
+    )
+}
+
 
 const projects = () => {
   return (
@@ -44,7 +73,9 @@ const projects = () => {
       </Head>
       <main className='w-full mb-16 flex flex-col items-center justify-center'> 
         <Layout className='pt-16'>
-            <AnimatedText text=" Imagination Trumps Knowledge!" />
+            <AnimatedText text=" Imagination Trumps Knowledge!" 
+            className='mb-16'
+            />
 
             <div className='grid grid-cols-12 gap-24 '>
                 <div className='col-span-12'>
@@ -63,20 +94,50 @@ const projects = () => {
 
 
                 <div className='col-span-6'>
-                    Project - 1
+                <Project title="Crypto Screener Application"
+                link="/"
+                type="Featured Project"
+                github="https://github.com/Kushal168"
+                img={projects1}
+                />
                 </div>
+
                 <div className='col-span-6'>
-                    Project - 2
+                    <Project title="Crypto Screener Application"
+                link="/"
+                type="Featured Project"
+                github="https://github.com/Kushal168"
+                img={projects1}
+                />
                 </div>
 
                 <div className='col-span-12'>
-                    Featured Project
+                <FeaturedProjects title="Crypto Screener Application"
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+                It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                local currency."
+                link="/"
+                type="Featured Project"
+                github="https://github.com/Kushal168"
+                img={projects1}
+
+                />t
                 </div>
                 <div className='col-span-6'>
-                    Project - 3
+                <Project title="Crypto Screener Application"
+                link="/"
+                type="Featured Project"
+                github="https://github.com/Kushal168"
+                img={projects1}
+                />
                 </div>
                 <div className='col-span-6'>
-                    Project - 4
+                <Project title="Crypto Screener Application"
+                link="/"
+                type="Featured Project"
+                github="https://github.com/Kushal168"
+                img={projects1}
+                />
                 </div>
             </div>
 
