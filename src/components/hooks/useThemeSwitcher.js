0cdf18834a64479a0dd28a41b35f909be02c7c6a
Kushal <Kushalgoyal168@gmail.com>
2023-09-12 22:00:57 +0530

    using media query

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const useThemeSwitcher = () => {
+
+    //css media feature
     const preferDarkQuery = "(prefer-color-scheme: dark)"
+    const[mode,setMode] = useState("")
+
+    useEffect(()=>{
+        const mediaQuery = window.matchMedia(preferDarkQuery)
+        //  storing user preference in local storage
+        const userPref = window.localStorage.getItem("theme")
+
+        const handleChange =()=>{
+            if(userPref){
+                let check = userPref ==='dark'? "dark" : "light"
+            }
+
+        }
+
+        mediaQuery.addEventListener("change",handleChange)
+
+    },[])
+
   return (
     <div>
       
