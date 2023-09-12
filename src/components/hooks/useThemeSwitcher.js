bda9a1e7cf489311c1463e52523e3e4cf5941e51
Kushal <Kushalgoyal168@gmail.com>
2023-09-12 21:50:59 +0530

    hook component

---
+++ src/components/hooks/useThemeSwitcher.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const useThemeSwitcher = () => {
+    const preferDarkQuery = "(prefer-color-scheme: dark)"
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default useThemeSwitcher
