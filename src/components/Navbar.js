9ef36ee7adefd595a2041c2655a9d09de9917d72
Kushal <Kushalgoyal168@gmail.com>
2023-09-13 23:17:20 +0530

    error 1.0

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,9 @@ const CustomLink = ({href,title,className =""})=>{
             absolute left-0 -bottom-0.5
             group-hover:w-full transition-[width] ease duration-300
             ${router.asPath === href ? 'w-full' : 'w-0'}
-            `}>
+            dark:bg-light
+            `}
+            >
                 &nbsp;
                 </span>
         </Link>
@@ -29,7 +31,9 @@ const Navbar = () => {
     const[mode,setMode] = useThemeSwitcher();
 
   return (
-    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+    <header className='w-full px-32 py-8 font-medium flex items-center justify-between
+    dark:text-light'
+    >
         <nav>
             <CustomLink href="/" title="Home" className='mr-4 font-semibold'/>
             <CustomLink href="/about" title="about" className='mx-4 font-semibold' />
@@ -44,7 +48,9 @@ const Navbar = () => {
             <motion.a href='http://discord.com' target='blank' whileHover={{y:-2}} className="w-6 mx -3" whileTap={{scale:2}}><Discord/></motion.a>
 
             <button onClick={()=>{setMode(mode === "light" ? "dark" : "light")}}
-            className='ml-3 flex items-center justify-center rounded-full p-1 '
+            // className='ml-3 flex items-center justify-center rounded-full p-1 '
+            className={`ml-3 flex items-center justify-center rounded-full p-1
+            ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}
             >
                 {/* {
                     mode === "dark" ? <SunIcon className="fill-dark"></SunIcon> : <MoonIcon className="fill-dark"></MoonIcon>
