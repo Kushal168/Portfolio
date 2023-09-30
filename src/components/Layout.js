b684ed5706edace4b6834b1a83afc6977d67129f
Kushal <Kushalgoyal168@gmail.com>
2023-09-30 18:12:41 +0530

    animated test responsive

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Layout = ({children,className=""}) => {
   return (
-    <div className={`w-full h-full inline-block z-0 bg-light dark:bg-dark dark:text-light p-32 ${className}`}>
+    <div className={`w-full h-full inline-block z-0 bg-light dark:bg-dark p-32 xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}>
      {children}
     </div>
+    // <div className={`w-full h-full inline-block z-0 bg-light dark:bg-dark dark:text-light p-32 ${className}`}>
+    //  {children}
+    // </div>
   )
 }
 
